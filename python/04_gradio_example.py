0d7b0d7aca694379b0ce6fcf7c9133bf55eac3cb
Flippchen <91947480+Flippchen@users.noreply.github.com>
2024-03-08 18:37:35 +0100

    Feature: Added redis client and embedder

---
+++ python/04_gradio_example.py
@@ -0,0 +1,19 @@
+import gradio as gr
+import numpy as np
+import pandas as pd
+import redis
+from redis.commands.search.query import Query
+from sentence_transformers import SentenceTransformer
+import plotly.graph_objects as go
+from sklearn.manifold import TSNE
+import ast
+
+# Connect to redis
+client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+
+res = client.ping()
+print("Connection successfully:", res)
+
+keys = sorted(client.keys("movies:*"))
+
+embedder = SentenceTransformer("msmarco-distilbert-base-v4")
