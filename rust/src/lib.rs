4f4fb278cc9e9ce4f061e5f52c159cee2739982d
Flippchen <91947480+Flippchen@users.noreply.github.com>
2024-03-13 21:39:22 +0100

    Feature: Deleted stuff

--- rust/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#[macro_use] extern crate log;
-
-pub mod model;
-pub use model::pooling::{Pooling, PoolingConfig};
-pub use model::bert::Bert;
-pub use model::sentence_tranformer::SentenceTransformer;
-
-
--- rust/src/model/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod pooling;
-pub mod bert;
-pub mod sentence_tranformer;
-
-pub use sentence_tranformer::SentenceTransformer;
-pub use bert::Bert;
-pub use pooling::{Pooling, PoolingConfig};
